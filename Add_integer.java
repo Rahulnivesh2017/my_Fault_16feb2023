571d196341b4564e6a6b43d1311fc1b159530a11
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-17 00:10:22 +0530

    Add_integer_two_number

---
+++ Add_integer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Add_integer
+{
+
+public static void main(String []args)
+{
+
+  int num1,num2,res;
+
+  Scanner sc=new Scanner(System.in);
+
+  System.out.println("Enter integer number one");
+
+  num1=sc.nextInt();
+
+  System.out.println("Enter integer number two");
+
+  num2=sc.nextInt();
+
+  res=num1+num2;
+
+  System.out.println("sum of two number " +res);
+
+  }
+
+  }
