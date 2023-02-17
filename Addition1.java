023be1a7c077103e811a3f515ec2228b052c3edb
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-17 10:27:53 +0530

    addition1

---
+++ Addition1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class Addition1{
+public static void main(String []args){
+
+System.out.println("Enter your  number" );
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=a+b;
+System.out.println("sum of two number =" +c);
+
+}
+}
