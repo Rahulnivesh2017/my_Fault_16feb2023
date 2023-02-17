a2279bc40a079775f7da6a10e28ddd99cecaaf11
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-17 10:08:33 +0530

    parimeter

---
+++ Parimeter.java
@@ -0,0 +1,20 @@
+class Parimeter
+{
+
+public static void main(String []args)
+{
+   int h=7,w=5,res,p;
+
+   System.out.println("Enter the value of rectangle");
+
+   res=h*w;
+
+   System.out.println("result is " +res);
+
+   System.out.println("Enter your parimeter");
+
+   p=2*(h+w);
+
+   System.out.println("result is " +p);
+   }
+   }
