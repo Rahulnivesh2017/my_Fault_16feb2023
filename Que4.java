ca092d30218e809e2fbfca90d89ea0850a43dc1a
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-20 00:23:50 +0530

    Que4

---
+++ Que4.java
@@ -0,0 +1,31 @@
+
+
+//write a java program to that accept 2 integers from the users and calci the sum of 2 integers
+
+import java.util.*;
+
+class Que4{
+
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the num");
+int a = sc.nextInt();
+
+System.out.println("Enter the num");
+int b = sc.nextInt();
+
+int c = a+b;
+
+System.out.println("Addition is "+c);
+
+
+
+
+
+
+}
+
+
+}
