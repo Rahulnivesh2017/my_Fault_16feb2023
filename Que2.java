bc4601520dd409adcaa3e26e7f7334ae7d00ded8
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-20 00:11:34 +0530

    Que2

---
+++ Que2.java
@@ -0,0 +1,21 @@
+
+
+//write a java program to to compute the parameter and area of the circle with the radius of 6 inch
+
+class Que2{
+
+public static void main(String [] args){
+
+
+int r = 6;
+
+float area =(float) 3.14*r*r;
+
+System.out.println("Area of circle is "+area);
+
+
+}
+
+
+
+}
