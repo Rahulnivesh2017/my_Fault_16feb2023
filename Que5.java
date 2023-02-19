0c80c0d22b537650ef7c23de061f82517199d219
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-20 00:17:38 +0530

    Que5

---
+++ Que5.java
@@ -0,0 +1,41 @@
+
+
+//write a java program  that accept 3 integer and find max of 3
+import java.util.*;
+
+class Que5{
+
+
+public static void main(String [] args){
+
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enetr the first num :");
+int a = sc.nextInt();
+
+System.out.println("Enetr the first num :");
+int b = sc.nextInt();
+
+System.out.println("Enetr the first num :");
+int c = sc.nextInt();
+
+if(a>b||a>c){
+
+	System.out.println("Greater Num is"+a);
+
+	}
+if(b>a||b>c){
+
+		System.out.println("Greater Num is"+b);
+	}
+
+
+
+}
+
+
+
+
+
+}
