467870647f7dcb323b0ca830a5dc76bd8ef2442c
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-20 00:05:32 +0530

    aiisg3_17FEB

---
+++ assig3_17FEB.java
@@ -0,0 +1,39 @@
+
+
+// write a program to accept 4 integer p,q,r,s from user where r&s are +ve and p is even if q>r and s>p and if sum of r&s >
+//the sum of p&q prinnt current value otherwise print wrong value
+
+import java.util.*;
+
+class assig3_17FEB{
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the num");
+int p = sc.nextInt();
+
+int q = sc.nextInt();
+int r = sc.nextInt();
+int s = sc.nextInt();
+
+if(r>0 && s>0){
+
+	int sum1 = r+s;
+
+
+
+}
+else{
+
+System.out.println("Enter +ve num");
+
+
+
+
+}
+
+System.out.println("Yes");
+}
+
+}
