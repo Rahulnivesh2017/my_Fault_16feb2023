a507fa7f7374e271ce0bb38056983205fec8eed7
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-19 23:38:07 +0530

    Que1

---
+++ Que1.java
@@ -0,0 +1,23 @@
+
+//write a java program to compute the parameter and area of rectangle with a height of 7 inch and with oif 5 inch
+import java.util.*;
+
+class Que1{
+
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Jai Hanuman");
+int a =7 , b = 5;
+System.out.println("Height is "+a+"Width is "+b);
+
+int c = a*b;
+
+
+System.out.println("Area is "+c);
+
+
+}
+
+}
